2ddde2e29d850391fc0c1899d88c58d87b835a55
Vani Dawar <vani.dawar07@gmail.com>
2025-08-28 22:26:32 +0530

    Add files via upload

---
+++ 100days.c
@@ -0,0 +1,11 @@
+//Q1: Write a program to input two numbers and display their sum.//
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("input 2 numbers");
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("sum is %d",sum);
+}
