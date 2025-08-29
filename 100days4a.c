ed105f6fc37d173607c0b6d6563bcc0621cffc43
Vani Dawar <vani.dawar07@gmail.com>
2025-08-29 23:25:26 +0530

    Add files via upload

---
+++ 100days4a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter 2 numbers");
+    scanf("%d %d",&a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("after swap:%d and %d",a,b);
+}
