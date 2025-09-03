ed9f521a3a020cdec3a59a628527bb0dfa9e3faf
Vani Dawar <vani.dawar07@gmail.com>
2025-09-03 22:24:51 +0530

    Update 100days11b.c

--- 100days11b.c
+++ 100days11b.c
@@ -1,4 +1,4 @@
-Q22: Write a program to find profit or loss percentage given cost price and selling price.
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.//
 
 #include <stdio.h>
 int main() {
