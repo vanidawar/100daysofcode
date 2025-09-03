22c7520296fc3cd8ceb1638600861aa5cfbda091
Vani Dawar <vani.dawar07@gmail.com>
2025-09-03 22:24:39 +0530

    Update 100days11b.c

--- 100days11b.c
+++ 100days11b.c
@@ -1,13 +1,11 @@
 Q22: Write a program to find profit or loss percentage given cost price and selling price.
 
 #include <stdio.h>
-
 int main() {
+    
     float costP, sellingP, percnt;
-
     printf("Enter Cost Price and Selling Price: ");
     scanf("%f %f", &costP, &sellingP);
-
     if (sellingP > costP) 
     {
         percentage = ((sellingP - costP) / costP) * 100;
