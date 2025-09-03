bf8fc91a38a9303052fc3ab6a41fbd20d0c0f87c
Vani Dawar <vani.dawar07@gmail.com>
2025-09-03 22:26:02 +0530

    Update 100days8b.c

--- 100days8b.c
+++ 100days8b.c
@@ -1,4 +1,4 @@
-Q16: Write a program to input three numbers and find the largest among them using if–else.
+//Q16: Write a program to input three numbers and find the largest among them using if–else.//
 
 #include <stdio.h>
 int main()
