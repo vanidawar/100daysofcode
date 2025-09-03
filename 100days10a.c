8f43f49f436a5b522f2cbb5e79ad20586efa578f
Vani Dawar <vani.dawar07@gmail.com>
2025-09-03 22:23:04 +0530

    Update 100days10a.c

--- 100days10a.c
+++ 100days10a.c
@@ -1,4 +1,4 @@
-Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+//Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.//
 
 #include <stdio.h>
 int main() 
