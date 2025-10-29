b178b7dd4dec4992358f42233ca0df368ab95e34
Vani Dawar <vani.dawar07@gmail.com>
2025-10-29 10:05:51 +0530

    Add files via upload

---
+++ 100days43a.c
@@ -0,0 +1,13 @@
+//Q85: Reverse a string.//
+
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    printf("enter a string");
+    fgets(str,sizeof(str),stdin);
+    while (str[i]!='\0')
+    {
+        
+    }
+}
